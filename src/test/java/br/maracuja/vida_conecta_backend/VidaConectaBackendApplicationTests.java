c4ee675f724730c577b3aa342fb9e4154fe4061b
edvaljunior <juniorronj@alu.ufc.br>
2026-08-31 20:41:46 -0300

    config: add configuration of the project

---
+++ src/test/java/br/maracuja/vida_conecta_backend/VidaConectaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.maracuja.vida_conecta_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaConectaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
